d07b419f69579544e9875bb999a737fdfac02800
ZakariaElkhadir <zelkhadir5@gmail.com>
2023-11-16 15:00:46 +0100

    edit

--- tools.c
+++ tools.c
@@ -82,3 +82,34 @@ void find_func(char *opcode, char *value, int ln, int format)
 	if (flag == 1)
 		err(3, ln, opcode);
 }
+
+void call_fun(op_func func, char *op, char *val, int ln, int format)
+{
+	stack_t *node;
+	int flg;
+	int i;
+
+	flg = 1;
+	if (strcmp(op, "push") == 0)
+	{
+		if (val != NULL && val[0] == '-')
+		{
+			val = val + 1;
+			flg = -1;
+		}
+		if (val == NULL)
+			err(5, ln);
+		for (i = 0; val[i] != '\0'; i++)
+		{
+			if (isdigit(val[i]) == 0)
+				err(5, ln);
+		}
+		node = create_node(atoi(val) * flg);
+		if (format == 0)
+			func(&node, ln);
+		if (format == 1)
+			add_to_queue(&node, ln);
+	}
+	else
+		func(&head, ln);
+}
